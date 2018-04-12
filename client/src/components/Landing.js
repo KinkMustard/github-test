9f8746fde5cad3e3f1d0de2b6fb80824bf07579f
KinkMustard <alec2001@gmail.com>
2018-04-11 22:21:43 -0600

    added billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Landing = () => (
+  <div style={{ textAlign: "center" }}>
+    <h1>Anime</h1>
+    Anime is the apex of human culture and innovation.
+  </div>
+);
+
+export default Landing;
---
+++ client/src/components/Payments.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Anime"
+        description="please pay for dank memes"
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">give me money</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(null, actions)(Payments);
