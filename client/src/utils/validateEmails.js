69bb9618ec063fc3b5e27a24a5b4802f91abef2a
KinkMustard <alec2001@gmail.com>
2018-04-14 12:36:19 -0600

    users can see survey results

---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,19 @@
+const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export default (emails) => {
+  let newEmails = emails;
+  if (emails.slice(-1) === ",") {
+    newEmails = newEmails.slice(0, -1);
+  }
+
+  const invalidEmails = newEmails
+    .split(",")
+    .map(email => email.trim())
+    .filter(email => re.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+
+  return null;
+};
